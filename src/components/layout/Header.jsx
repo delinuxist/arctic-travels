569e3132998a2673375bbcaa8142910ce2922e2d
delinuxist <delinuxist.gmail.com>
2022-05-05 07:47:32 +0000

    initial setup

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div><img src="" alt="" /></div>
+      <div>header</div>
+    </div>
+  )
+}
+
+export default Header
